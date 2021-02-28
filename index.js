4ea72dc892c09daa2b385ba8c3c52605eb6524db
Abhijit Muley <abhijitrmly@gmail.com>
2021-03-01 00:04:36 +0530

    use env for yandex api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-cond-assign */
+require('dotenv').config();
+
 const needle = require('needle');
 
 const fileUrl = 'http://norvig.com/big.txt';
-const APIkey = 'dict.1.1.20210216T114936Z.e4989dccd61b9626.373cddfbfb8a3b2ff30a03392b4e0b076f14cff9';
+const APIkey = process.env.YANDEX_API_KEY;
 
 const getWordDataFromApi = (word) => new Promise((resolve) => needle.get(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${APIkey}&lang=en-en&text=${word}`, (error, response) => {
   if (error) throw error;
